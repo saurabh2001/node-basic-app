6799988a960c8c109cb2239a8cd95c8cd2877032
saurabh <saury.shah@gmail.com>
2019-06-23 13:07:34 +0530

    adding endpoints in request

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ node .\app.js
 
 #Run the http server on 3000 : 
 node .\serverapp.js
+
+Go to the browser and hit http://localhost:3000/ping
--- serverapp.js
+++ serverapp.js
@@ -1,6 +1,19 @@
 const http = require("http");
+const logger = require('./logger');
 
-const server = http.createServer();
+const server = http.createServer((req,res)=>{
+    if(req.url === '/')
+    {
+        logger.printMessage('/ called');
+        res.write('/ called');
+        res.end();
+    }else if(req.url === '/ping'){
+
+        logger.printMessage('Ping successful');
+        res.write('Ping successful');
+        res.end();
+    }
+});
 server.listen(3000);
 console.log('Server listening on port 3000...');
 
