9b47b426f58202eba66b4885b52889a2ddca8506
saurabh <saury.shah@gmail.com>
2019-06-23 13:00:46 +0530

    adding http module

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # node-basic-app
 
 node version 10
+
+
+#Run the logger test
+node .\app.js
+
+#Run the http server on 3000,
+node .\serverapp.js
---
+++ serverapp.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer();
+server.listen(3000);
+console.log('Server listening on port 3000...');
+
+server.on('connection',()=>{
+console.log('connection initiated');
+});
